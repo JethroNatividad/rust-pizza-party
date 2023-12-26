5fb52d176b9bf5f706a158105b24047823aad4ec
JethroNatividad <natividad.jet@gmail.com>
2023-12-26 21:07:10 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// Write a program to evenly divide pizzas. Prompt for the
+// number of people, the number of pizzas, and the number of
+// slices per pizza. Ensure that the number of pieces comes out
+// even. Display the number of pieces of pizza each person
+// should get. If there are leftovers, show the number of leftover
+// pieces
+
+// A program to evenly divide pizzas based on how many people, and slices per pizza.
+// Inputs: n_people, n_pizza, n_slices.
+// Process: 
+// Output: slice_per_person, leftovers
+
+// How many people? 8
+// How many pizzas do you have? 2
+// How many slices per pizza? 8
+
+// 8 people sharing 2 pizzas with 8 slices each.
+// Each person gets 2 slices of pizza.
+// There are 0 leftover slices of pizza.
+
+
 fn main() {
     println!("Hello, world!");
 }
